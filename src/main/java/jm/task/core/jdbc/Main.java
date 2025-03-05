bd795ad431078814f2c4b032dfd4a00e8c89457d
nazarovman2024 <nazarovman2024@gmail.com>
2025-03-06 00:22:18 +0300

    250306_JPP_1.1.3_fix_0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,31 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Name0", "Lastname0", (byte) 0);
+        System.out.println("User с именем Name0 добавлен в базу данных");
+        userService.saveUser("Name1", "Lastname1", (byte) 10);
+        System.out.println("User с именем Name1 добавлен в базу данных");
+        userService.saveUser("Name2", "Lastname2", (byte) 100);
+        System.out.println("User с именем Name2 добавлен в базу данных");
+        userService.saveUser("Name3", "Lastname3", (byte) 33);
+        System.out.println("User с именем Name3 добавлен в базу данных");
+
+        System.out.println("\nВсе пользователи в базе:");
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        System.out.println("\nТаблица очищена");
+
+        userService.dropUsersTable();
+        System.out.println("Таблица удалена");
     }
 }
